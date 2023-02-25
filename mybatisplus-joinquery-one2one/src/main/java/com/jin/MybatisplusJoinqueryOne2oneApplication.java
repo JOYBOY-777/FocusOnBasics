0cc2f590075b8c3339990d8e134635aaacc022b1
LIFEIYU <1781073841@qq.com>
2023-02-25 19:52:09 +0800

    mybatis初始化

---
+++ mybatisplus-joinquery-one2one/src/main/java/com/jin/MybatisplusJoinqueryOne2oneApplication.java
@@ -0,0 +1,13 @@
+package com.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusJoinqueryOne2oneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplusJoinqueryOne2oneApplication.class, args);
+    }
+
+}
